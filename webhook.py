46963e9c7b0f22d460a9abb74480a5fdcd3ae58e
Angel.Seda <angel.seda@nordstrom.com>
2022-09-19 09:38:59 -0500

    Add response

--- webhook.py
+++ webhook.py
@@ -1,10 +1,13 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 @app.route('/', methods=['POST'])
 
 def webhook():
     print(request.json)
+    peticion = request.json
+    if peticion['queryResult']['intent']['displayName'] == 'Receta':
+        return jsonify({'fulfillmentText':'Receta de platano encontrada'})
     return 'HOLA'
 
 if __name__ =='__main__':
